754859c76874786e0a63e23cee3f7f9bae413a6a
Jóhann Björn Björnsson <jbjornsson@deloitte.dk>
2023-03-16 15:35:10 +0100

    cleaning up

--- ApplicationCode/Api/Program.cs
+++ ApplicationCode/Api/Program.cs
@@ -20,7 +20,7 @@ app.UseSwagger(opt =>
     {
         opt.PreSerializeFilters.Add((swagger, httpReq) =>
         {
-            var serverUrl = $"http://{httpReq.Host}/api";
+            var serverUrl = $"http://{httpReq.Host}/";
             swagger.Servers = new List<OpenApiServer> { new() { Url = serverUrl } };
         });
     });
